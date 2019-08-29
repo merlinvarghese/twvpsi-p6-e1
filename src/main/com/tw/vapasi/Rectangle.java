5ff163380a75a7d126cf2d88fd7d67c0294df0f7
Vijayalakshmi Velur <velur.viju@gmail.com>
2019-08-29 11:10:47 +0530

    [Vij,Randeep]Add. Design changes for Square and Rectangle Calculations

--- src/main/com/tw/vapasi/Rectangle.java
+++ src/main/com/tw/vapasi/Rectangle.java
@@ -3,21 +3,35 @@ package com.tw.vapasi;
 /**
  * Understands a four-sided flat shape where every angle is a right angle.
  */
+
 class Rectangle {
     private double length;
     private double breadth;
 
-    Rectangle(double length, double breadth) {
+    private Rectangle(double length, double breadth) {
         this.length = length;
         this.breadth = breadth;
+    }
+
+    private Rectangle(double side) {
+        this.length = side;
+        this.breadth = side;
+    }
+
+    public static Rectangle createSquare(double side) {
+        return new Rectangle(side);
+    }
+
+    public static Rectangle createRectangle(double length, double breadth) {
+        return new Rectangle(length, breadth);
     }
 
     double area() {
         return length * breadth;
     }
 
-    double perimeter(int TWO) {
-        return TWO * (length + breadth);
+    double perimeter() {
+        return 2 * (length + breadth);
     }
 }
 
--- src/test/com/tw/vapasi/RectangleTest.java
+++ src/test/com/tw/vapasi/RectangleTest.java
@@ -1,31 +1,67 @@
 package com.tw.vapasi;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
 class RectangleTest {
-    @Test
-    void expectArea15ForLength5Breadth3() {
-        Rectangle rectangle = new Rectangle(5.0, 3.0);
-        assertEquals(15.0, rectangle.area());
+
+    @Nested
+    class RectangleCalculator {
+        @Test
+        void expectArea15ForLength5Breadth3() {
+            Rectangle rectangle = Rectangle.createRectangle(5.0, 3.0);
+            assertEquals(15.0, rectangle.area());
+        }
+
+        @Test
+        void expectArea32ForLength8Breadth4() {
+            Rectangle rectangle = Rectangle.createRectangle(8.0, 4.0);
+            assertEquals(32.0, rectangle.area());
+        }
+
+        @Test
+        void expectPerimeter16ForLength4Breadth2() {
+            Rectangle rectangle = Rectangle.createRectangle(4.0, 2.0);
+            assertEquals(12.0, rectangle.perimeter());
+        }
+
+        @Test
+        void expectPerimeter16ForLength5Breadth3() {
+            Rectangle rectangle = Rectangle.createRectangle(5.0, 3.0);
+            assertEquals(16.0, rectangle.perimeter());
+        }
     }
 
-    @Test
-    void expectArea32ForLength8Breadth4() {
-        Rectangle rectangle = new Rectangle(8.0, 4.0);
-        assertEquals(32.0, rectangle.area());
+    @Nested
+    class SquareCalculator {
+
+        @Test
+        void expectArea16ForSide4() {
+            Rectangle square = Rectangle.createSquare(4);
+            Assertions.assertEquals(16, square.area());
+        }
+
+        @Test
+        void expectArea9ForSide3() {
+            Rectangle square = Rectangle.createSquare(3);
+            Assertions.assertEquals(9, square.area());
+        }
+
+        @Test
+        void expectPerimeter8ForSide() {
+            Rectangle square = Rectangle.createSquare(4);
+            Assertions.assertEquals(16, square.area());
+        }
+
+        @Test
+        void expectPerimeter9ForSide3() {
+            Rectangle square = Rectangle.createSquare(3);
+            Assertions.assertEquals(9, square.area());
+        }
     }
 
-    @Test
-    void expectPerimeter16ForLength4Breadth2() {
-        Rectangle rectangle = new Rectangle(4.0, 2.0);
-        assertEquals(12.0, rectangle.perimeter(2));
-    }
-
-    @Test
-    void expectPerimeter16ForLength5Breadth3() {
-        Rectangle rectangle = new Rectangle(5.0, 3.0);
-        assertEquals(16.0, rectangle.perimeter(2));
-    }
 }
